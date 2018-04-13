474750f7d5869bf3f61650db2adf820b8566b86a
oweson <@oweson570347720qq.com>
2018-04-13 17:17:41 +0800

    又添加了新的练习，主要是file和collections;

---
+++ java/src/javas/TestRandom.java
@@ -0,0 +1,13 @@
+package javas;
+
+import java.util.Random;
+
+public class TestRandom {
+
+	public static void main(String[] args) {
+	//	System.out.println(Random()+"hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
