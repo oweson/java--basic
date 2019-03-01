ae18ae59c094674996e577de1530aed74455ec4e
oweson <570347720@qq.com>
2019-03-01 22:18:23 +0800

    基础很薄弱，练习；无论如何，开心，准备打持久战！

---
+++ java/src/eclipse/mar/extendsuperthisabstract/demo09/Fu.java
@@ -0,0 +1,7 @@
+package eclipse.mar.extendsuperthisabstract.demo09;
+
+public class Fu {
+
+    int num = 30;
+
+}
---
+++ java/src/eclipse/mar/interfaceandmanystate/demo02/Zi.java
@@ -0,0 +1,4 @@
+package eclipse.mar.interfaceandmanystate.demo02;
+
+public class Zi extends Fu implements MyInterface {
+}
