be406bd64f45af8d5216ad48f8b57cb20ed4e2a2
oweson <570347720@qq.com>
2019-04-04 23:37:23 +0800

    一眨眼就入职4day了，一点都不顺，今天上午还在搞环境； 公司的框架很垃圾，部门太垃圾，没人带，学不到东西，自己的选择后悔了吧； 走一步看一步；

---
+++ java/src/top/sea521/algorithm/nangchang/JieCheng.java
@@ -0,0 +1,23 @@
+package top.sea521.algorithm.nangchang;
+
+/**
+ * the class is create by @Author:oweson
+ *
+ * @Date：2019/3/23 0023 23:48
+ */
+public class JieCheng {
+    public static final int demo1(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * demo1(n - 1);
+
+    }
+
+    public static void main(String[] args) {
+        int i = demo1(5);
+        System.out.println(i);
+
+
+    }
+}
