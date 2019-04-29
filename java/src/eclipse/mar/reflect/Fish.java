b99797ac50a3b09311d4565303f53172698a2e53
oweson <570347720@qq.com>
2019-04-29 22:52:32 +0800

    我和她；

---
+++ java/src/eclipse/mar/reflect/Fish.java
@@ -0,0 +1,12 @@
+package eclipse.mar.reflect;
+
+/**
+ * the class is create by @Author:oweson
+ *
+ * @Date：2019/4/29 0029 21:32
+ */
+public class Fish {
+    public static final void demo1(){
+        System.out.println("fish!");
+    }
+}
