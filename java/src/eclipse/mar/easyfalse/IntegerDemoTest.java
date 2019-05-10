ba03b86c5e26510a946419aa02ad43a869818a7c
oweson <570347720@qq.com>
2019-05-10 10:55:17 +0800

    添加jui并发模块；

---
+++ java/src/eclipse/mar/easyfalse/IntegerDemoTest.java
@@ -0,0 +1,9 @@
+package eclipse.mar.easyfalse;
+
+/**
+ * the class is create by @Author:oweson
+ *
+ * @Date：2019/5/10 0010 10:03
+ */
+public class IntegerDemoTest {
+}
---
+++ juc/src/com/atguigu/juc/TestAtomicDemo.java
@@ -0,0 +1,66 @@
+package com.atguigu.juc;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 一、i++ 的原子性问题：i++ 的操作实际上分为三个步骤“读-改-写”
+ * int i = 10;
+ * i = i++; //10
+ * <p>
+ * int temp = i;
+ * i = i + 1;
+ * i = temp;
+ * <p>
+ * 二、原子变量：在 java.util.concurrent.atomic 包下提供了一些原子变量。
+ * 1. volatile 保证内存可见性
+ * 2. CAS（Compare-And-Swap） 算法保证数据变量的原子性
+ * CAS 算法是硬件对于并发操作的支持
+ * CAS 包含了三个操作数：
+ * ①内存值  V
+ * ②预估值  A
+ * ③更新值  B
+ * 当且仅当 V == A 时， V = B; 否则，不会执行任何操作。
+ */
+public class TestAtomicDemo {
+
+    public static void main(String[] args) {
+
+        AtomicDemo ad = new AtomicDemo();
+        int serialNumber = ad.getSerialNumber();
+        System.out.println(serialNumber);
+        System.out.println(ad.serialNumber);
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(ad).start();
+        }
+    }
+
+}
+
+class AtomicDemo implements Runnable {
+
+//	private volatile int serialNumber = 0;
+
+    public AtomicInteger serialNumber = new AtomicInteger(0);
+
+
+    @Override
+    public void run() {
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+        }
+
+        System.out.println(getSerialNumber());
+    }
+
+    /**
+     * 得到然后自增1；
+     */
+    public int getSerialNumber() {
+        return serialNumber.getAndIncrement();
+    }
+
+
+}
