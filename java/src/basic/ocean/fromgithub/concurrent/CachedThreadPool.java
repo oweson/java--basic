1109818bca1740acd185a7184965a1a55aa3856c
oweson <570347720@qq.com>
2019-05-19 15:49:24 +0800

    我错了；

---
+++ java/src/basic/ocean/fromgithub/concurrent/CachedThreadPool.java
@@ -0,0 +1,17 @@
+package basic.ocean.fromgithub.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by brian on 16/11/30.
+ */
+public class CachedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new LiftOff());
+        }
+        exec.shutdown();
+    }
+}
---
+++ java/src/basic/ocean/fromgithub/multithread/meet/Run2_StartVsRun.java
@@ -0,0 +1,46 @@
+package basic.ocean.fromgithub.multithread.meet;
+
+/**
+ * Created by brian on 2016/4/11.
+ */
+
+/**
+ * myThread2.start()和myThread2.run()的区别
+ */
+
+class MyThread2 extends Thread {
+    public MyThread2() {
+        System.out.println("构造方法打印:" + Thread.currentThread().getName());
+    }
+
+    @Override
+    public void run() {
+        System.out.println("run方法打印：" + Thread.currentThread().getName());
+    }
+}
+
+public class Run2_StartVsRun {
+    public static void main(String[] args) {
+        MyThread2 myThread2 = new MyThread2();
+        myThread2.start();
+        //myThread2.run();知识一个主线程，普通的方法！
+    }
+}
+
+
+
+/*
+ myThread2.start();
+//myThread2.run();
+输出：
+构造方法打印:main
+run方法打印：Thread-0
+
+--------------------
+//myThread2.start();
+myThread2.run();
+输出：
+构造方法打印:main
+run方法打印：main
+
+*/
