c0a21ed288bfd461d0e4b63d89af9557da7b2189
皮皮程 <570347720@QQ.com>
2019-07-19 23:49:19 +0800

    明天不知道

---
+++ java/src/basic/ocean/apigson/Hate.java
@@ -0,0 +1,16 @@
+package basic.ocean.apigson;
+
+/**
+ * the class is create by @Author:oweson
+ *
+ * @Date：2019/7/19 12:00
+ */
+public class Hate {
+    public static void main(String[] args) {
+        double a=1142.86;
+        float b=21f;
+        System.out.println(a*7+1142.84 );
+        // 9142.859999999999
+
+    }
+}
