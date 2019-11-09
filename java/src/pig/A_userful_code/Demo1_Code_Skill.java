d86393689a163999bdd267cbcd9328ac036555d9
oweson <570347720@qq.com>
2019-11-10 00:04:19 +0800

    温故而知新，可以为大爷！

---
+++ java/src/pig/A_userful_code/Demo1_Code_Skill.java
@@ -0,0 +1,36 @@
+package pig.A_userful_code;
+
+/**
+ * the class is create by @Author:oweson
+ *
+ * @Date：2019/11/9 12:01
+ */
+public class Demo1_Code_Skill {
+    public static void main(String[] args) {
+        // 0 内部函数尽量使用基础类型，避免了隐式封装类型的打包和拆包；
+        //
+        //内部函数参数使用基础类型，用语法上避免了内部函数的参数空指针判断；
+        //
+        //内部函数返回值使用基础类型，用语法上避免了调用函数的返回值空指针判断。
+        // 1 不能使用一个值为null的引用类型变量来调用非静态方法，否则会抛出异常
+        // 保证返回的数组和列表不为null, 避免调用函数的空指针判断。
+        String str = null;
+        if (str.equals("SnailClimb")) {
+        } else {
+        }
+        // 2 BigDecimal 的大小比较
+        //
+        //a.compareTo(b)
+        // 3 WebSocket 的用途
+        //
+        //凡是涉及到即时通讯的，基本上都能用上它：
+        //
+        //网页上的在线聊天
+        //多人在线游戏
+        //在线股票网站
+        //在线即时新闻网站
+        //高清视频流
+        //应用集群之间的通信
+        //远程系统/软件的状态和性能的实时监控
+    }
+}
---
+++ java/src/pig/A_userful_code/Demo2_Fast_Code.java
@@ -0,0 +1,60 @@
+package pig.A_userful_code;
+
+/**
+ * the class is create by @Author:oweson
+ *
+ * @Date：2019/11/9 12:10
+ */
+public class Demo2_Fast_Code {
+    public static void main(String[] args) {
+        int i = 10;
+        // 1 10.var
+        String s=null;
+        // 2 s.null/ s.notnull/s.nn
+        if (s == null) {
+
+        }
+        if (s != null) {
+
+        }
+        if (s != null) {
+
+        }
+        // 3 list.for  // list.fori 后者是带索引的遍历
+
+        // 4 取反操作  a.not
+        boolean a=false;
+        boolean b = !a == false;
+        // 4 if条件判断 b.if
+        if (b) {
+
+        }
+        // s.length>0.if
+        if (s.length()>0) {
+
+        }
+        // 5 强转换
+        Object c=100;
+        Integer integer = (Integer) c;
+        demo2(c);
+        demo3(100);
+
+
+
+
+    }
+
+    private static void demo3(int i) {
+
+    }
+
+    private static void demo2(Object c) {
+
+    }
+
+    public int demo1(){
+        // return 测试；
+        return 100;
+
+    }
+}
