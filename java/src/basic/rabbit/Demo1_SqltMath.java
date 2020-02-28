4cb10cad55c79ceeeb05d206a6eafc477c01c4bd
oweson <570347720@qq.com>
2020-02-28 21:12:38 +0800

    add

---
+++ java/src/basic/rabbit/Demo1_SqltMath.java
@@ -0,0 +1,13 @@
+package basic.rabbit;
+
+/**
+ * the class is create by @Author:oweson
+ *
+ * @Date：2020/2/28 16:47
+ */
+public class Demo1_SqltMath {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(100.0));
+
+    }
+}
