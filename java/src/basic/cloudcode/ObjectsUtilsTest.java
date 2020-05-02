a68d5a1ded33e6feac2d98d4c3c9380591d1354e
oweson <570347720@qq.com>
2020-05-02 21:40:36 +0800

    add

---
+++ java/src/basic/cloudcode/ObjectsUtilsTest.java
@@ -0,0 +1,14 @@
+package basic.cloudcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class ObjectsUtilsTest {
+    public static void main(String[] args) {
+        String s = null;
+        // 新的判空工具类
+        System.out.println(Objects.equals(s, null));
+    }
+}
